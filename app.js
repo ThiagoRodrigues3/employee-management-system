9e056af96f1c6381df857aa8aaffdc05c5689f17
Thiago Rod <whytigo@gmail.com>
2022-03-15 02:50:50 -0400

    fixed issue

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Lacrosse21!",
+    password: "root",
     database: "employee_management_db"
 });
 
